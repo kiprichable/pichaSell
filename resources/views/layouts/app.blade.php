071ccd091fa21c9e73853a82388823be2f0e4af3
Daniel <dcheruiy@d.umn.edu>
2018-02-26 16:35:45 -0600

    landing page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Picha Sell</title>
+    <!-- Overrides and Custom Styles -->
+    <link rel="stylesheet" href="{{ URL::Asset('css/app.css') }}">
+    <![endif]-->
+</head>
+
+@include('partials.header')
+@include('partials.nav')
+@include('partials.content')
+@include('partials.footer')
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</body>
+</html>
